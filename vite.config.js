32d13a6b6415543838d0ee9bbeca17da603298e5
Mah-Maroofi <maroofi.mahtab@gmail.com>
2025-03-11 15:09:22 +0100

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kws2100-map-app/', 
+  base: '/kws2100-map-app/', // VIKTIG for GitHub Pages
 });
