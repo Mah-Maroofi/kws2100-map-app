be669c3af7210dd00a31cfdbee2c33ab4895adf8
Mah-Maroofi <maroofi.mahtab@gmail.com>
2025-03-11 15:02:09 +0100

    Fixed GitHub Pages issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kws2100-map-app/', // Viktig for GitHub Pages!
+  base: '/kws2100-map-app/', 
 });
