7324c75063801ad1f9ed9d287422a9e31d9e0ab6
Mah-Maroofi <maroofi.mahtab@gmail.com>
2025-03-11 15:25:31 +0100

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kws2100-map-app/', // VIKTIG for GitHub Pages
+  base: '/kws2100-map-app/',
+  build: {
+    outDir: 'dist', 
+  },
 });
